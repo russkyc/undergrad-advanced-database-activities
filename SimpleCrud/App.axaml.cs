d5cd56e2bd658ccc3134f152812b189c9da20242
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-11-18 02:29:50 +0800

    :recycle: Use new hosting container

--- SimpleCrud/App.axaml.cs
+++ SimpleCrud/App.axaml.cs
@@ -1,7 +1,12 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Infrastructure.Data;
+using MongoFramework;
+using Russkyc.DependencyInjection.Implementations;
 using SimpleCrud.Services;
+using SimpleCrud.ViewModels;
 using SimpleCrud.Views;
 
 namespace SimpleCrud;
@@ -16,11 +21,18 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var container = AppHost.Build();
-        
+        var connection = MongoDbConnection.FromConnectionString(Environment.GetEnvironmentVariable("MONGODB_URI"));
+        var container = ApplicationHost<MainWindow>.CreateDefault()
+            .ConfigureServices(services =>
+            {
+                services.Add(new DbContext(connection))
+                    .AddSingleton<DialogService>()
+                    .AddTransient<StudentManagementViewModel>();
+            });
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = container.Resolve<MainWindow>();
+            desktop.MainWindow = container.Root;
         }
 
         base.OnFrameworkInitializationCompleted();
